f2d77622444eb3b434395dde45a23d2bd1ca4bef
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-01-27 07:21:21 +0700

    Fix Bug
    Fix bug with the current `comment-service` plugin.
    
    https://github.com/tovic/comment-service-plugin-for-mecha-cms/releases/tag/v1.0.1

--- comment-allow/launch.php
+++ comment-allow/launch.php
@@ -1,9 +1,10 @@
 <?php
 
-if($config->page_type === 'article') {
-    Weapon::add('chunk_lot_before', function() {
+if($config->is->post) {
+    Weapon::add('shield_lot_before', function() {
         $config = Config::get();
-        $allow = isset($config->article->fields->allow_comments) && $config->article->fields->allow_comments !== false;
+        $s = $config->page_type;
+        $allow = isset($config->{$s}->fields->allow_comments) && $config->{$s}->fields->allow_comments !== false;
         $config->comments->allow = $allow;
         Config::set('comments.allow', $allow);
         Shield::lot(array('config' => $config));
